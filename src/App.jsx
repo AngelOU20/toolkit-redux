cd9108dfdd5a4b1eb1cdc90c1c6408549e7f351e
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-18 16:48:25 -0500

    included decrement action

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { increment } from "./store/slices/counter";
+import { decrement, increment } from "./store/slices/counter";
 
 function App() {
   const { counter } = useSelector((state) => state.counter);
@@ -12,18 +12,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="#">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="#">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>React Toolkit and React Redux</h1>
       <div className="card">
-        <h2 className="card-title">{counter}</h2>
+        <h2 className="card-title">Count is: {counter}</h2>
         <div className="card-buttons">
           <button onClick={() => dispatch(increment())}>Increment value</button>
+          <button onClick={() => dispatch(decrement())}>Decrement value</button>
         </div>
       </div>
     </>
