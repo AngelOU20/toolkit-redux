eec1b3c302da495ac4d55af874a4af538db0d400
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-20 11:00:35 -0500

    Added PokemonApp.jsx

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,13 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>Pokemon App</h1>
+      <hr />
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
+    </>
+  );
+};
