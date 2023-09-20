1b216966ee5389a3db244e1c609ed57fc670582e
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-20 12:43:52 -0500

    show pokemons paginated

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,13 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemons } from "./store/slices"; // thunks
+
 export const PokemonApp = () => {
+  const {
+    isLoading,
+    pokemons = [],
+    page,
+  } = useSelector((state) => state.pokemons);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, [dispatch]);
+
   return (
     <>
       <h1>Pokemon App</h1>
       <hr />
+      <h2>{page}</h2>
+      <span>Loading: {isLoading ? "true" : "false"}</span>
+
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
       </ul>
+
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
     </>
   );
 };
