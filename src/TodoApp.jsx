e2bd0eed66c712e477657ed3b9f0a82e7d020d58
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-25 15:01:44 -0500

    update TodoApp

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,9 +1,22 @@
+import { useGetTodosQuery } from "./store/apis";
+
 export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+  console.log(todos);
+
   return (
     <>
       <h1>Todos- RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? "true" : "false"}</h4>
+
+      <ol>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong>{todo.completed ? "Done" : "Pending"}</strong> {todo.title}
+          </li>
+        ))}
+      </ol>
 
       <pre>...</pre>
       <button>Next Todo</button>
